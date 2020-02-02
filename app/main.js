839c06ae12070bbcf0ecfd28b8e15c91d52cff73
yuriygumeniuk <yuriygumeniuk@gmail.com>
2020-02-02 16:00:14 +0200

    main.js

---
+++ app/main.js
@@ -0,0 +1,11 @@
+// document.getElementById('.center_titel').onclick = function() {
+// document.getElementById('main').classList.add('main--main-bg');
+// this..classList.remove('main--main-bg');
+// }
+
+var btns = document.getElementsByClassName('center_titel');
+btns[0].onclick = function() {
+	alert('afdads');  
+}
+
+// document.getElementById('id').classList.add('class');
